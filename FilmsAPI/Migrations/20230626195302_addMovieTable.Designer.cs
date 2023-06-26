82992dd6b67fb0af08f8919b8daba9dd2423025b
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-26 22:54:05 +0300

    create table in database

--- FilmsAPI/Migrations/20230626195302_addMovieTable.Designer.cs
+++ FilmsAPI/Migrations/20230626195302_addMovieTable.Designer.cs
@@ -3,6 +3,7 @@ using FilmsAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FilmsAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230626195302_addMovieTable")]
+    partial class addMovieTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
