d4969bae7fd29ee9bda1b2964cd0e22b59985ff3
wanderlust-li <zhekalichm4n@icloud.com>
2023-07-18 22:30:12 +0300

    add header in website

---
+++ movie-web/src/Components/Pages/MainPage.jsx
@@ -0,0 +1,15 @@
+﻿import React from "react";
+import Header from "../Layout/Header"
+
+class MainPage extends React.Component {
+    
+    render() {
+        return (
+            <div>
+                <Header/>
+            </div>
+        );
+    };
+};
+
+export default MainPage;
