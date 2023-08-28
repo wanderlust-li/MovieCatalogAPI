1d267c793765a37f472c5bd30b298c82d0b24ac8
wanderlust-li <zhekalichm4n@icloud.com>
2023-08-28 14:47:49 +0300

    create movieActorController

---
+++ MovieAPI/Controllers/v1/MovieActorController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MovieAPI.Models;
+using MovieAPI.Repository.IRepository;
+
+namespace MovieAPI.Controllers.v1;
+
+
+[Route("api/v{version:apiVersion}/Actor")]
+[ApiController]
+[ApiVersion("1.0")]
+public class MovieActorController : Controller
+{
+    private readonly APIResponse _response;
+    private readonly IMovieActorRepository _db;
+    private readonly IMapper _mapper;
+
+    public MovieActorController(IMovieActorRepository db, IMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+        this._response = new();
+    }
+    
+    
+    
+}
