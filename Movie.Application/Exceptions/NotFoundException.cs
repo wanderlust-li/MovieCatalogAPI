54b38285889975f4d33cf5f2a19bee692cf92b29
wanderlust-li <zhekalichm4n@icloud.com>
2023-10-29 02:52:42 +0300

    add exceptions

---
+++ Movie.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Movie.Application.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string name, object key) : base($"{name} ({key}) was not found")
+    {
+            
+    }
+}
